0d82214525cc334e8501db91ccd0fcc77dfa55b7
Wad-Laptop <iamwad@gmail.com>
2025-01-22 14:42:40 +0700

    ทดสอบทำให้เกิด bug เพิื่อทำการ pull rebase

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Sequelize, Model } from 'sequelize';
+
+const sequelize = (sequelize, DataTypes) => {
+  class AppsVersion extends Model {
+
+    static associate (models) {
+
+    }
+  }
+}
 
 const index = () => {
   return (
     <View>
       <Text>index</Text>
+      <Text>{{sequelize}}</Text>
     </View>
   )
 }
